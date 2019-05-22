ea26656a7a2bc3841fa4c9b2bb26ba3ae8cb1600
rjairath <rishabhjairath@gmail.com>
2019-05-22 23:47:02 +0530

    weather app basic

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/node
+
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = process.env.WEATHER_API_KEY;
+let city = argv.c || "delhi";
+
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+request(url, function(err, response, body){
+    if(err){
+        console.log("error:", err);
+    }
+    else{
+        let weather = JSON.parse(body);
+        if(weather.message == "city not found"){
+            console.log("city not found");
+            return;
+        }
+        console.log(`It's ${weather.main.temp} in ${weather.name}`);
+    }
+});
